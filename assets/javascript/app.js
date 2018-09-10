481aeed8bda86cee414aa798d0dbbd7e9aed50ea
David Hiller <HillerDavid@users.noreply.github.com>
2018-09-09 21:36:50 -0400

    Need to fix letter replacement in blanks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,48 @@
 console.log('App starting')
+
+// Variable to determine whether game has begun
+
+var gameStarted = false;
+
 // - Create a list of words
 
 var wordArray = ["Nicole", "Alyssa", "Mike", "David", "Jean", "Michael", "MiChel", "Josue"]
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
 var lettersGuessed = []
-
+var chosenWord = ""
 
 // # Start the application!
-document.onkeyup = function (event) {
-    var displayWords = document.getElementById("display-words");
-    var randomWord = wordGenerator()
 
-    var tempString = "<p>"
-    for (var i = 0; i < randomWord.length; i++) {
-        if (lettersGuessed.includes(randomWord[i].toLowerCase)) {
-            tempString += randomWord[i] + " ";
+document.onkeyup = function (event) {
+    var userGuess = event.key
+
+    //New Game
+    if (gameStarted === false) {
+        gameStarted = true
+        chosenWord = wordGenerator().toLowerCase()
+        var blankWord = chosenWord.replace(/[a-z]/g, "_ ")
+        console.log("Chosen Word: " + chosenWord + "blankWord: " + blankWord)
+        document.getElementById("display-words").innerHTML = blankWord
+    } else {
+
+        //Determine if userGuess was previously made
+        if (lettersGuessed.includes(userGuess)) {
+            alert("You already guessed " + userGuess + "!")
         } else {
-            tempString += "_ "
+            lettersGuessed.push(userGuess)
+            console.log("letter pushed")
+        }
+
+        //Determine whether userGuess is correct
+        for (var i = 0; i < chosenWord.length; i++) {
+            if (chosenWord[i] === userGuess) {
+                console.log("correct " + tempString)
+            } else {
+                console.log("incorrect" + tempString)
+            }
         }
 
     }
-
-    tempString += "</p>"
-    displayWords.innerHTML = tempString
 }
 
 // - Select one randomly (Random Word Generator)
